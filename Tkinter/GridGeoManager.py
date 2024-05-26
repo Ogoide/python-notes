c8690d8384e6793c4208212524c1d7a7c689a4ab
Diogo Fernandes <135901907+Ogoide@users.noreply.github.com>
2024-05-26 16:53:17 +0100

    Add GridGeoManager.py

--- Tkinter/GridGeoManager.py
+++ Tkinter/GridGeoManager.py
@@ -10,7 +10,18 @@ import tkinter as tk
 
 window = tk.Tk()
 
-for r in range(11):
+for r in range(5):
+    """
+    Configuration of rows and columns to add responsive window resizing behaviour:
+    
+    Index: The index of the grid column or row that you want to configure or a list of indices to configure multiple rows or columns at the same time
+    Weight: A keyword argument called weight that determines how the column or row should respond to window resizing, relative to the other columns and rows
+    Minimum Size: A keyword argument called minsize that sets the minimum size of the row height or column width in pixels
+
+    """
+    window.columnconfigure(index=r, weight=2, minsize=50)
+    window.rowconfigure(index=r, weight=2, minsize=50)
+
     for c in range(5):
         frame = tk.Frame(
             master=window,
@@ -18,8 +29,9 @@ for r in range(11):
             borderwidth=1
         )
 
-        frame.grid(row=r, column=c)
+        # Padding adds extra spaces in between widgets
+        frame.grid(row=r, column=c, padx=5, pady=5)
         label = tk.Label(master=frame, text=f'Row {r}\nColumn {c}')
-        label.pack()
+        label.pack(padx=5, pady=5)
 
 window.mainloop()
